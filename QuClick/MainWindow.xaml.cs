537f91bf150390d2b4ace453d05bfbb2aaf5aa22
Dawid Grad <dawidgrad.work@gmail.com>
2019-05-10 21:33:03 +0100

    Added basic UI

---
+++ QuClick/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuClick
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void StartStopButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void StartStopKeybind_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ToggleKeybind_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FixKeybind_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
