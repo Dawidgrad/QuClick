4b5481bb319e9e79d3345f257f30fa3ad0b64c4a
Dawid Grad <dawidgrad.work@gmail.com>
2019-05-11 21:45:36 +0100

    Implemented global hook to enable application to detect key presses even if the window is not focused

---
+++ QuClick/Classes/KeyHandler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows;
+
+namespace QuClick.Classes
+{
+    public class KeyHandler
+    {
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        private int key;
+        private IntPtr hWnd;
+        private int id;
+
+        public KeyHandler(int key)
+        {
+            this.key = (int)key;
+            this.hWnd = new WindowInteropHelper(Application.Current.MainWindow).Handle;
+            id = this.GetHashCode();
+        }
+
+        public override int GetHashCode()
+        {
+            return key ^ hWnd.ToInt32();
+        }
+
+        public bool Register()
+        {
+            return RegisterHotKey(hWnd, 9000, 0, key);
+        }
+
+        public bool Unregiser()
+        {
+            return UnregisterHotKey(hWnd, id);
+        }
+
+
+    }
+}
--- QuClick/MainWindow.xaml.cs
+++ QuClick/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -9,6 +10,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -22,28 +24,33 @@ namespace QuClick
     public partial class MainWindow : Window
     {
         private bool canClick = true;
-        SettingsSingleton settings;
+        private SettingsSingleton settings;
+        private KeyHandler startStopHandler;
+        private KeyHandler toggleHandler;
+
+
+        [DllImport("User32.dll")]
+        private static extern bool RegisterHotKey(
+            [In] IntPtr hWnd,
+            [In] int id,
+            [In] uint fsModifiers,
+            [In] uint vk);
+
+        [DllImport("User32.dll")]
+        private static extern bool UnregisterHotKey(
+            [In] IntPtr hWnd,
+            [In] int id);
+
+        private HwndSource _source;
+        private const int HOTKEY_ID = 9000;
 
         public MainWindow()
         {
             InitializeComponent();
             settings = SettingsSingleton.GetInstance();
-            KeyDown += GetKey_KeyDownEvent;
         }
 
-        private void GetKey_KeyDownEvent(object sender, KeyEventArgs e)
-        {
-            if (settings.startStopKeybind == e.Key)
-            {
-                MouseClicker mouseClicker = new MouseClicker();
-                mouseClicker.ClickMouse();
-            }
-            else if (settings.toggleKeybind == e.Key)
-            {
-
-            }
-        }
-
+        // Detect when the Start / Stop button was clicked
         private async void StartStopKeybind_Click(object sender, RoutedEventArgs e)
         {
             if (canClick)
@@ -58,6 +65,7 @@ namespace QuClick
             }
         }
 
+        // Detect when the toggle button was clicked
         private async void ToggleKeybind_Click(object sender, RoutedEventArgs e)
         {
             if (canClick)
@@ -72,15 +80,24 @@ namespace QuClick
             }
         }
 
+        // Save Toggle keybind when key press is detected
         private void Toggle_PreviewKeyDown(object sender, KeyEventArgs e)
         {
             settings.toggleKeybind = e.Key;
             ToggleLabel.Text = e.Key.ToString();
+
+            //toggleHandler = new KeyHandler(settings.toggleKeybind);
+            //toggleHandler.Register();
         }
+
+        // Save Start / Stop keybind when key press is detected
         private void StartStop_PreviewKeyDown(object sender, KeyEventArgs e)
         {
             settings.startStopKeybind = e.Key;
             StartStopLabel.Text = e.Key.ToString();
+
+            //startStopHandler = new KeyHandler(settings.startStopKeybind);
+            //bool result = startStopHandler.Register();
         }
 
         private void FixKeybind_Click(object sender, RoutedEventArgs e)
@@ -88,6 +105,68 @@ namespace QuClick
 
         }
 
+        private void FrequencyButton_Click(object sender, RoutedEventArgs e)
+        {
 
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            var helper = new WindowInteropHelper(this);
+            _source = HwndSource.FromHwnd(helper.Handle);
+            _source.AddHook(HwndHook);
+            RegisterHotKey();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            _source.RemoveHook(HwndHook);
+            _source = null;
+            UnregisterHotKey();
+            base.OnClosed(e);
+        }
+
+        private void RegisterHotKey()
+        {
+            var helper = new WindowInteropHelper(this);
+            const uint VK_F10 = 0x79;
+            if (!RegisterHotKey(helper.Handle, HOTKEY_ID, 0, VK_F10))
+            {
+                // handle error
+            }
+        }
+
+        private void UnregisterHotKey()
+        {
+            var helper = new WindowInteropHelper(this);
+            UnregisterHotKey(helper.Handle, HOTKEY_ID);
+        }
+
+        private IntPtr HwndHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            const int WM_HOTKEY = 0x0312;
+            switch (msg)
+            {
+                case WM_HOTKEY:
+                    switch (wParam.ToInt32())
+                    {
+                        case HOTKEY_ID:
+                            OnHotKeyPressed();
+                            handled = true;
+                            break;
+                    }
+                    break;
+            }
+            return IntPtr.Zero;
+        }
+
+        private void OnHotKeyPressed()
+        {
+            MouseClicker mouseClicker = new MouseClicker();
+            mouseClicker.ClickMouse();
+        }
     }
+
+
 }
